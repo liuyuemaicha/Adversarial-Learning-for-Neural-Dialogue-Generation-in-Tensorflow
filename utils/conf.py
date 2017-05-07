7cf125d68e7c1226ba7af0269170959cb9d96119
六月麦茬 <1137108895@qq.com>
2017-05-07 20:56:01 +0800

    Update conf.py

--- utils/conf.py
+++ utils/conf.py
@@ -1,15 +1,15 @@
 __author__ = 'MORAN01'
 import os
 class disc_config(object):
-    batch_size = 1
+    batch_size = 12
     lr = 0.1
     lr_decay = 0.6
-    vocabulary_size = 20000
+    vocabulary_size = 2000
     embed_dim = 128
     hidden_neural_size = 128
-    hidden_layer_num = 1
-    train_dir = 'data/subj0.pkl'
-    max_len = 40
+    hidden_layer_num = 4
+    data_dir = 'disc_data/'
+    max_len = 20
     valid_num = 100
     checkpoint_num = 1000
     init_scale = 0.1
@@ -19,7 +19,7 @@ class disc_config(object):
     max_decay_epoch = 30
     max_grad_norm = 5
     out_dir = os.path.abspath(os.path.join(os.path.curdir,"runs"))
-    checkpoint_every = 10
+    checkpoint_every = 100
 
 class gen_config(object):
     beam_size = 5
@@ -27,13 +27,11 @@ class gen_config(object):
     learning_rate_decay_factor = 0.99
     max_gradient_norm = 5.0
     batch_size = 1
-    size = 12
-    num_layers = 2
-    vocab_size = 10000
-    data_dir = "./train_data/"
-    train_dir = "./train_data/"
+    emb_dim = 1000
+    num_layers = 1
+    vocab_size = 1000
+    data_dir = "./gen_data/"
+    train_dir = "./gen_data/"
     max_train_data_size = 0
-    steps_per_checkpoint = 200
+    steps_per_checkpoint = 100
     buckets = [(5, 10), (10, 15), (20, 25), (40, 50), (50, 50)]
-
-
