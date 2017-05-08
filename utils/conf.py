a888f805bac4ec859ac8eb567ac876a08a06f09c
六月麦茬 <1137108895@qq.com>
2017-05-08 16:51:07 +0800

    Update conf.py

--- utils/conf.py
+++ utils/conf.py
@@ -1,4 +1,4 @@
-__author__ = 'MORAN01'
+__author__ = 'liuyuemaicha'
 import os
 class disc_config(object):
     batch_size = 12
@@ -9,7 +9,9 @@ class disc_config(object):
     hidden_neural_size = 128
     hidden_layer_num = 4
     data_dir = 'disc_data/'
-    max_len = 20
+    max_len = 50
+    #query_len = 0
+
     valid_num = 100
     checkpoint_num = 1000
     init_scale = 0.1
@@ -20,13 +22,14 @@ class disc_config(object):
     max_grad_norm = 5
     out_dir = os.path.abspath(os.path.join(os.path.curdir,"runs"))
     checkpoint_every = 100
+    buckets = [(5, 10), (10, 15), (20, 25), (40, 50), (50, 50)]
 
 class gen_config(object):
     beam_size = 5
     learning_rate = 0.5
     learning_rate_decay_factor = 0.99
     max_gradient_norm = 5.0
-    batch_size = 1
+    batch_size = 4
     emb_dim = 1000
     num_layers = 1
     vocab_size = 1000
@@ -35,3 +38,4 @@ class gen_config(object):
     max_train_data_size = 0
     steps_per_checkpoint = 100
     buckets = [(5, 10), (10, 15), (20, 25), (40, 50), (50, 50)]
+
